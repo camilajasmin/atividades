06aee2b2d38cde6e2154974d5e9ccdabb980c126
camila <camilajasmin.ec@gmail.com>
2023-11-28 03:08:15 +0000

    first commit

---
+++ exe1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    FILE *even, *odds;
+    int n = 10;
+    size_t k = 0;
+
+    even = fopen("even.txt", "w");
+    odds = fopen("odds.txt", "w");
+
+    for (k = 1 ; k < n + 1; k++)
+    {
+        k%2 == 0 ? fprintf(even, "\t%5\n", k)
+                 : fprintf(odds, "\t%5\n", k);
+    }
+
+    fclose(even);
+    fclose(odds);
+
+    return 0;
+    
+
+
+}
